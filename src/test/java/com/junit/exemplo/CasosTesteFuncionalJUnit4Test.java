bf53064e5b44f5fa92d204196f908a941af9e4bd
AndreAbe <andvicente@gmail.com>
2012-09-05 23:36:13 -0300

    Renomeando CasoTesteFuncionalJUnit4 com sufixo Test

--- src/test/java/com/junit/exemplo/CasosTesteFuncionalJUnit4Test.java
+++ src/test/java/com/junit/exemplo/CasosTesteFuncionalJUnit4Test.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CasosTesteFuncionalJUnit4 {
+public class CasosTesteFuncionalJUnit4Test {
 
 	@Before
 	public void setUp() throws Exception {
